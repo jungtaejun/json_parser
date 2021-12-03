23debb8837d4e2e749ad40667680e36fdd24cdd8
AnSangHyeon_ <a01033630274@gmail.com>
2021-12-03 17:58:10 +0900

    [SH] 완성본

--- sh/index.js
+++ sh/index.js
@@ -1,7 +1,20 @@
-import data from"./../data/works.json";
-console.log(data)
+// $(document).ready(function(){
+//    console.log(works) 
+// }  
 
-// var sh = [];
-// for(i=1;i<=57;i++) {
-//     sh.push(i)
-// }
+$(document).ready(function() {
+    const elements = []
+    for(const row of works) {
+        const element = `
+            <div class="row">
+                <div class="col-1">`+row.category+`</div>
+                <div class="col-3">`+row.title+`</div>
+                <div class="col-4">`+row.subTitle+`</div>
+                <div class="col-1">`+row.all+`</div>
+                <div class="col-3">`+row.url+`</div>
+            </div>
+        `
+        elements.push(element)
+    } 
+    $('.list-body').html(elements)
+});
