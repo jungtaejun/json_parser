2a8f134d87e8e4b22c1bf3d354e2896970ccb3a9
taejun427 <jungtaejun09@gmail.com>
2021-12-03 12:12:57 +0900

    json 파일 출력하는 반복문 작성

--- tj/index.js
+++ tj/index.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
   console.log(works)
+
+  var html =[];
+
+    for(var i = 0; i < works.length; i++) {
+      html.push(works[i].category + ' ');
+      html.push(works[i].title + ' ');
+      html.push(works[i].subTitle + ' ');
+      html.push(works[i].all + ' ');
+      html.push(works[i].url + ' ');
+      html.push('<br>')
+      html.push('<br>')
+    }
+    $('.data').html(html.join(''));
 });
 
