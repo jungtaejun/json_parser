a0ef7eead0beae9d1e359e7a69b2b791a357b7a1
taejun427 <jungtaejun09@gmail.com>
2021-12-16 11:20:14 +0900

    for문 3개 구분

--- tj/index.js
+++ tj/index.js
@@ -1,21 +1,37 @@
-$(document).ready(function(){
-  console.log(works)
+function makeList(index, work) {
+  var element = []
 
+  if(index % 2 === 0){
+    element.push('<div class="row">')
+  } else {
+    element.push('<div class="row bg-gray">')
+  }
+  element.push('<div class="col-1">' + work.category + '</div>')
+  element.push('<div class="col-3">' + work.title + '</div>')
+  element.push('<div class="col-4">' + work.subTitle + '</div>')
+  element.push('<div class="col-1">' + work.all + '</div>')
+  element.push('<div class="col-3">' + work.url + '</div>')
+  element.push('</div>')
+  return element.join('')
+}
+
+$(document).ready(function(){
   var html =[];
+  
+  for(var j=0; j < works.length; j++) {
+    var work = works[j]
+    // html.push(makeList(j, work))
+  }
+  
+  for (var key in works) {
+    var work = works[key]
+    html.push(makeList(key, work))
+  }
+
   var i=0;
   for(var work of works) {
-    if(i%2 === 0){
-      html.push('<div class="row">')
-    }
-    else {
-      html.push('<div class="row bg-gray">')
-    }
-    html.push('<div class="col-1">' + work.category + '</div>')
-    html.push('<div class="col-3">' + work.title + '</div>')
-    html.push('<div class="col-4">' + work.subTitle + '</div>')
-    html.push('<div class="col-1">' + work.all + '</div>')
-    html.push('<div class="col-3">' + work.utl + '</div>')
-    html.push('</div>')
+    console.log(makeList(i, work))
+    // html.push(makeList(i, work))
     i++
   }
   $('.data').html(html.join(''))
